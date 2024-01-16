b745ffad8ac82c48c3d5bfe628422dc90cfbef73
laksanautama <kussa_laksana@uhnsugriwa.ac.id>
2024-01-16 23:58:11 +0800

    submit file cpp and pdf

---
+++ distribusi_uang.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <set>
+
+using namespace std;
+
+//----kode di atas batas ini tidak boleh diganti atau ditambah----
+
+
+void tampil_jml_karyawan(){
+
+//CODE
+}
+
+int main(){
+
+return 0;
+}
